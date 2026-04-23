e1a44ceead60aa5cbd0d5c3ee2b0fd0bf7d9891a
Uli <up@rmp.software>
2026-04-23 08:24:17 +0200

    v2

---
+++ assets/index.astro_astro_type_script_index_0_lang.DW37nBRU.js
@@ -0,0 +1,1 @@
+import{r as a}from"./utils.CkUu8Q7C.js";a(".page-header h1",-2,2);
---
+++ assets/utils.CkUu8Q7C.js
@@ -0,0 +1,1 @@
+function n(t,e=-4,o=4){document.querySelectorAll(t).forEach(r=>{let a=Math.random()*(o-e)+e;r.style.transform=`rotate(${a}deg)`})}export{n as r};
